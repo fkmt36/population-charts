81c63ee9f648cea245aa5e393077ac50625abd51
Ryota Fukumoto <happyfukumoto@gmail.com>
2021-06-24 03:31:33 +0900

    Added base scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,11 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [{ src: '~/assets/scss/style.scss' }],
+
+  styleResources: {
+    scss: ['~/assets/scss/_variable.scss'],
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
