1ac283ccf26de366eb2f12dc257f10a137247b9d
Ryota Fukumoto <happyfukumoto@gmail.com>
2021-06-24 22:46:46 +0900

    Added @nuxtjs/style-resources to modules in nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,6 +44,7 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/style-resources',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
