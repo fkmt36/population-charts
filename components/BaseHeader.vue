45998588933196fe503a3574babc13c0f3a4baa5
Ryota Fukumoto <happyfukumoto@gmail.com>
2021-06-24 23:20:16 +0900

    Created BaseHeader component

---
+++ components/BaseHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+header {
+  @include flex(center, center);
+  background: cl(primary);
+  height: 50px;
+  border-bottom: 1px solid cl(grey);
+  h1 {
+    font-size: fs(h3);
+  }
+}
+</style>
