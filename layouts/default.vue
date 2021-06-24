ff96261aff09eb77b08bdcbf191ca7088b3b9261
Ryota Fukumoto <happyfukumoto@gmail.com>
2021-06-24 23:20:40 +0900

    Add BaseHeader to Default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,23 @@
 <template>
   <div>
-    <Nuxt />
+    <base-header :title="title"></base-header>
+    <main><Nuxt /></main>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    title() {
+      return document ? document.title : ''
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+main {
+  margin: 20px 0;
+  padding: 0 15px;
+}
+</style>
