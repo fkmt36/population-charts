f7f065ae9c32038bb236cff087911229d427e4ab
Ryota Fukumoto <happyfukumoto@gmail.com>
2021-06-26 14:22:17 +0900

    Added error page

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="error">
+    <h1>{{ error.statusCode }}</h1>
+    <p v-if="error.statusCode === 404">ページが見つかりません</p>
+    <p v-else>エラーが発生しました</p>
+    <nuxt-link to="/">トップに戻る</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.error {
+  @include flex(center, center);
+  flex-direction: column;
+  p {
+    margin: 15px 0;
+  }
+}
+</style>
