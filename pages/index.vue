192b50fe16835b0287adc63732cdd4d823559bb9
Ryota Fukumoto <happyfukumoto@gmail.com>
2021-06-27 23:00:00 +0900

    Componentized prefecture list

--- pages/index.vue
+++ pages/index.vue
@@ -4,17 +4,7 @@
     <section class="pref-section">
       <div class="pref-section-inner">
         <h2>都道府県</h2>
-        <ul class="pref-list">
-          <li v-for="(v, k) in prefectures" :key="'pref-' + k">
-            <base-checkbox :label="v" :check="(c) => selectPref(k, c)" />
-          </li>
-          <li
-            v-for="n of Object.keys(prefectures).length > 0
-              ? Object.keys(prefectures).length - 1
-              : 0"
-            :key="'empty-' + n"
-          ></li>
-        </ul>
+        <the-prefecture-list />
       </div>
     </section>
     <!-- 人口構成グラフ -->
@@ -26,30 +16,14 @@
   </div>
 </template>
 <script>
-import { mapState, mapMutations, mapActions } from 'vuex'
+import { mapActions } from 'vuex'
 
 export default {
-  computed: {
-    ...mapState(['prefectures', 'populations', 'selectedPrefs']),
-  },
   async created() {
     await this.fetchPrefectures()
   },
   methods: {
-    ...mapMutations(['addSelectedPrefs', 'removeSelectedPrefs']),
-    ...mapActions(['fetchPrefectures', 'fetchPopulationByPrefCode']),
-
-    // selectPref 選択した都道府県をselectedPrefに追加（削除）
-    async selectPref(prefCode, checked) {
-      if (checked) {
-        // 人口データがなければfetchする
-        if (!(prefCode in this.populations))
-          await this.fetchPopulationByPrefCode(prefCode)
-        this.addSelectedPrefs(prefCode)
-      } else {
-        this.removeSelectedPrefs(prefCode)
-      }
-    },
+    ...mapActions(['fetchPrefectures']),
   },
 }
 </script>
@@ -63,18 +37,6 @@ export default {
     h2 {
       font-size: fs(normal);
       margin-bottom: 10px;
-    }
-
-    .pref-list {
-      @include flex(center, unset);
-      flex-wrap: wrap;
-      li {
-        width: 90px;
-        margin-bottom: 6px;
-        &:empty {
-          margin-bottom: 0;
-        }
-      }
     }
   }
 }
